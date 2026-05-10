25e074d8b5d653eb959ca1db9dedd8e9f6557ade
krishnaharshap <103913630+krishnaharshap@users.noreply.github.com>
2026-05-10 03:24:23 -0600

    feat: add phase 2 API testing integration

--- src/main/java/com/automation/pages/ProductsPage.java
+++ src/main/java/com/automation/pages/ProductsPage.java
@@ -1,9 +1,11 @@
 package com.automation.pages;
 
 import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
